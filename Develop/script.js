b5d90b6ad5a685a88d908f10258beee9ca5f3959
Connor Raper-Mateo <connor.rapermateo@student.csulb.edu>
2023-03-13 20:52:07 -0700

    added main files

---
+++ Develop/script.js
@@ -0,0 +1,24 @@
+// Assignment Code
+var generateBtn = document.querySelector("#generate");
+
+function generatePassword(){
+    const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()_+~`|}{[]\\:;?><,./-=';
+    let result = '';
+    const charactersLength = characters.length;
+    for (let i = 0; i < 8; i++) {
+      result += characters.charAt(Math.floor(Math.random() * charactersLength));
+    }
+    return result;
+  }
+
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+
+}
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
